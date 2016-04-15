b75e657825858bb16564ed58ab137e4e06475b17
Tablesalt <samchott@gmail.com>
2016-04-14 23:56:42 -0500

    added some models? Also a simple view

--- djangosite/bookr/models.py
+++ djangosite/bookr/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+#idk about on_delete, might have made some mistakes there.
+#probably helps that there is no way to delete users
+class Book(models.Model):
+	picture = models.ImageField()
+	seller = models.ForeignKey('User', on_delete=models.CASCADE)
+	price = models.FloatField()
+	condition = models.CharField(max_length=100)
+	isbn = models.BigIntegerField()
+
+class User(models.Model):
+	avatar = models.ImageField()
+	name = models.CharField(max_length=20)
+	email = models.EmailField()
+	phone = models.CharField(max_length=20)
+
+class Review(models.Model):
+	#these related_names might cause issues?????!?!?!?!!!
+	rater = models.ForeignKey('User', on_delete=models.CASCADE, related_name='raters')
+	ratee = models.ForeignKey('User', on_delete=models.CASCADE, related_name='ratees')
+	stars = models.IntegerField()
+	text = models.CharField(max_length=500)
